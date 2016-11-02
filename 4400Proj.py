61428f54a9b4baae2eb9fa935b0d130608fc2ea9
Kimberly <kwolford3@gatech.edu>
2016-11-02 16:23:48 -0400

    Added register page and ME page. Created some functions for pages called by register and ME.

--- 4400Proj.py
+++ 4400Proj.py
@@ -1,5 +1,5 @@
 from tkinter import *
-import pymysql
+#import pymysql
 import os
 from re import findall
 
@@ -34,7 +34,64 @@ class CS4400:
 
     def Register(self):
         print("Registered")
-        
+        #Creates registration window
+        self.registerWin = win
+        self.registerWin.title("Register")
+        #create username label
+        userLab= Label(self.registerWin,text="Username:")
+        userLab.grid(row=0,column=0)
+        #create email label
+        emailLab= Label(self.registerWin,text="GT Email Address:")
+        emailLab.grid(row=1,column=0)
+        #create password label
+        passLab = Label(self.registerWin, text = "Password:")
+        passLab.grid(row=2, column=0)
+        #create confirm password label
+        confLab = Label(self.registerWin, text = "Confirm Password:")
+        confLab.grid(row=3, column=0)
+        #create username text field
+        self.user = StringVar()
+        userEnt =Entry(self.registerWin, width =30, textvariable = self.user)
+        userEnt.grid(row=0, column =1)
+        #create email text field
+        self.email = StringVar()
+        emailEnt =Entry(self.registerWin, width =30, textvariable = self.email,)
+        emailEnt.grid(row =1, column =1)
+        #create password text field
+        self.passw = StringVar()
+        passEnt = Entry(self.registerWin, width=30, textvariable = self.passw,)
+        passEnt.grid(row=2, column=1)
+        #create confirm password text field
+        self.conf = StringVar()
+        confEnt = Entry(self.registerWin, width=30, textvariable = self.conf,)
+        confEnt.grid(row=3, column=1)
+        #Creates a create button
+        #Left as redirecting to login page. Can change if needed.
+        self.createBut =Button(self.registerWin, text="Create", command = self.Login)
+        self.createBut.grid(row=4,column=0)
+
+    #ME PAGE
+    def Me_page(self):
+        #Creates ME window
+        self.meWin = win
+        self.meWin.title("ME")
+        self.profBut = Button(self.meWin, text = "Edit Profile", command = self.Edit_profile)
+        self.profBut.grid(row=0, column=0)
+        self.applBut = Button(self.meWin, text = "My Applications", command = self.My_app)
+        self.applBut.grid(row=1, column=0)
+        self.backBut = Button(self.meWin, text = "Back", command = self.Main_page)
+        self.backBut.grid(row=2, column=0)
+
+    def Main_page(self):
+        print ("main page")
+
+    def Edit_profile(self):
+        print ("edit profile")
+
+    def My_app(self):
+        print ("my apps")
+
+    
 
 win = Tk()
 app = CS4400(win)
